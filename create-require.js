c8e09834e322d8a106ac8018011f799e2fed03f2
pooya parsa <pooya@pi0.ir>
2020-06-12 12:55:52 +0200

    fix: use fake path if filename is directory

--- create-require.js
+++ create-require.js
@@ -1,7 +1,13 @@
 const nativeModule = require('module')
 const path = require('path')
+const fs = require('fs')
 
 function createRequire (filename) {
+  // If filename is dir, createRequire goes with parent directory, so we need fakepath
+  if (isDir(filename)) {
+    filename = path.join(filename, 'index.js')
+  }
+
   // Added in Node v12.2.0
   if (nativeModule.createRequire) {
     return nativeModule.createRequire(filename)
@@ -23,6 +29,16 @@ function _createRequire (filename) {
   mod.paths = nativeModule.Module._nodeModulePaths(path.dirname(filename))
   mod._compile('module.exports = require;', filename)
   return mod.exports
+}
+
+function isDir (path) {
+  try {
+    const stat = fs.lstatSync(path)
+    return stat.isDirectory()
+  } catch (e) {
+    // lstatSync throws an error if path doesn't exist
+    return false
+  }
 }
 
 module.exports = createRequire
--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const path = require('path')
 const t = require('tap')
 const createRequire = require('../create-require')
 
-const _require = createRequire(path.join(__dirname, 'some/index.js'))
+const _require = createRequire(path.join(__dirname, 'some'))
 const e = _require('./file')
 t.equal(e, 'someFile')
 t.end()
